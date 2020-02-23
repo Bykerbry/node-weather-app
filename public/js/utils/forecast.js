983c676ff02c52793cf2ef0c6f84d56aa5a27760
Bryce <bykerbry@umich.edu>
2020-02-23 13:42:26 -0500

    Forecast now includes daily summary and daily high/low

--- public/js/utils/forecast.js
+++ public/js/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (lat, long, callback) => {
         } else if (body.error) {
             callback(body.error, undefined)
         } else {
-            callback(undefined, `It is currently ${body.currently.summary}. It's ${body.currently.temperature}℉, and there is a ${body.currently.precipProbability}% chance of rain.`
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature.toFixed(1)}℉, and there is a ${body.currently.precipProbability}% chance of precipitation. High: ${body.daily.data[0].temperatureHigh.toFixed(1)}℉, Low: ${body.daily.data[0].temperatureLow.toFixed(1)}℉`
             )
         }
     })
